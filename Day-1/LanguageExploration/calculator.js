b824dc36f7392de051b8a287fc8d99a4dced0ae8
tkmagesh <tkmagesh77@gmail.com>
2014-05-21 12:50:50 +0530

    Solution - Part - 1

--- Day-1/LanguageExploration/calculator.js
+++ Day-1/LanguageExploration/calculator.js
@@ -1,6 +1,11 @@
 function add(x,y){
 	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		if (n instanceof Array) return add.apply(this,n);
 		return !isNaN(n) ? parseInt(n,10) : 0;	
 	}
-	return parseArg(x) + parseArg(y);
+	var result = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
 }
