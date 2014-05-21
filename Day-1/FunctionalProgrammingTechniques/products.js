5a47a77ec3f5cab3d66a992edcab392f89d4d726
tkmagesh <tkmagesh77@gmail.com>
2014-05-21 16:23:16 +0530

    Functional Programming Techniques

---
+++ Day-1/FunctionalProgrammingTechniques/products.js
@@ -0,0 +1,8 @@
+var products =[
+	{id :1, name :"pen", cost :10, units:1},
+	{id :2, name :"hen", cost :60, units:8},
+	{id :6, name :"ten", cost :30, units:3},
+	{id :4, name :"len", cost :80, units:7},
+	{id :9, name :"den", cost :30, units:4},
+	{id :5, name :"zen", cost :90, units:2}
+]
